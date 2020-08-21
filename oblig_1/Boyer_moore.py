fad285bf62d5464c2fe2438c5e773a1ca985f14f
h577870 <kristoffer.davidsen@gmail.com>
2020-08-21 11:39:16 +0200

    added counter per char, a few bug fixes

--- oblig_1/Boyer_moore.py
+++ oblig_1/Boyer_moore.py
@@ -1,7 +1,7 @@
 import PyPDF2
 import string
 
-# TODO: Finn en fornuftig måte å telle antall sammenligninger på.
+# Bruker kanskje litt lang tid på å søke? Én årsak er at alfabetet inkluderer diverse tegn utover det norske...
 
 file = open('et_dukkehjem.pdf', 'rb')
 pdf_reader = PyPDF2.PdfFileReader(file)
@@ -9,15 +9,16 @@ pages = pdf_reader.numPages
 
 
 # indekserer string ved bruk av dictionary. Legg til karakterer som ikke finnes i dict dersom programmet feiler,
-# i 'alphabet_dict. Lesson learned; Ikke bruk fortellinger med gammelt språk.
+# i 'alphabet_dict'. Lesson learned; Ikke bruk fortellinger med gammelt språk.
 def index_string(p: str):
-	alphabet_dict = dict.fromkeys(string.printable + "œæøå\néè", -1)
+	alphabet_dict = dict.fromkeys(string.printable + "œæøå\néè«™", -1)
 	
 	# Kan legge inn en sjekk for duplikater, men unødvendig når p er/skal være 5 karakterer lang.
 	for i in p:
 		d = {i: p.rfind(i)}
 		alphabet_dict.update(d)
 	return alphabet_dict
+
 
 def BMAlgorithm(t, p):
 	dict_temp = index_string(pattern)
@@ -28,39 +29,39 @@ def BMAlgorithm(t, p):
 	counter = 0
 	
 	while i < n:
+		counter += 1
 		if t[i] == p[j]:
-			counter += 1
 			if j == 0:
-				# print(counter)
+				print('Snitt pr. karakter: %s ' % (counter / i))
 				return i
 			else:
 				i -= 1
 				j -= 1
 		else:
 			foo = dict_temp.get(t[i])
-			print(t[i])
+			# Kommenter ut denne dersom programmet feiler for å finne karakter i debug-mode.
+			# print(t[i])
 			i = i + m - min(j, 1 + foo)
 			j = m - 1
-	# print(counter)
+	print('Snitt pr. karakter: %s ' % (counter / i))
 	return -1
 
 
 def init():
-	file_length = 0
 	pdf_string = ""
 	for ii in range(pages):
 		page = pdf_reader.getPage(ii)
-		file_length += len(page.extractText())
 		pdf_string += page.extractText().lower()
 	return pdf_string
 
 
 if __name__ == "__main__":
 	def show(t, p):
-		print('Text: %s' % t)
 		ps = BMAlgorithm(t, p)
-		print('Match: %s%s' % ('.'*ps, p))
+		print('Match: i = %s' % ps)
+		print('Pattern: %s' % p)
 	
-	pattern = "vidunderlig"
+	
+	pattern = "skyde"  # Sett inn mønster her...
 	text = init()
 	show(text, pattern)
